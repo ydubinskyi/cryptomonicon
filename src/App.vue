8802fb7ce06f6e36b76962ab5cf423f2b8dcf47d
Yaroslav Dubinskyi <slavabilaz@gmail.com>
2021-06-04 00:03:06 +0200

    separate preloader component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,6 @@
 <template>
   <div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
-    <div
-      class="fixed w-100 h-100 opacity-80 bg-purple-800 inset-0 z-50 flex items-center justify-center"
-      v-if="!isCoinListLoaded"
-    >
-      <svg
-        class="animate-spin -ml-1 mr-3 h-12 w-12 text-white"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-      >
-        <circle
-          class="opacity-25"
-          cx="12"
-          cy="12"
-          r="10"
-          stroke="currentColor"
-          stroke-width="4"
-        ></circle>
-        <path
-          class="opacity-75"
-          fill="currentColor"
-          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-        ></path>
-      </svg>
-    </div>
+    <app-preloader v-if="!isCoinListLoaded" />
 
     <div class="container">
       <section>
@@ -195,9 +171,12 @@
 
 <script>
 import { subscribeToTicker, unsubscribeFromTicker, getCoinList } from "./api";
+import AppPreloader from "./components/AppPreloader";
 
 export default {
   name: "App",
+
+  components: { AppPreloader },
 
   data() {
     return {
